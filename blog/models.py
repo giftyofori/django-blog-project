a8d3e8bb186cd80e60134fe2d8dc73b01dd73984
AITI Student <aiti-student@AITI-82.(none)>
2012-07-05 17:22:23 +0000

    my new baby

--- blog/models.py
+++ blog/models.py
@@ -18,10 +18,27 @@ class Comment(models.Model):
     updated= models.DateField()
     post= models.ForeignKey(Post)
     
+    def body_first_60(self):
+	return self.body[:60]
 
     def __unicode__(self):
 	return self.body
+class CommentInline(admin.TabularInline):
+    model= Comment
+
+class PostAdmin(admin.ModelAdmin):
+    list_display= ('title','created','updated')
+    search_fields=('title','body')
+    list_filter=('created',)
+    inlines =[
+	CommentInline,
+    ]
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display =('author','body_first_60','created','updated')
+    list_filter=('created', 'author')
 
 # Create your models here.
-admin.site.register(Post)
-admin.site.register(Comment)
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment,CommentAdmin)
